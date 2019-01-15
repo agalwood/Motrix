f5b30efc7d4bd21d6e70f2f734ffa237e82878e7
Dr_rOot <agalwood.net@gmail.com>
2019-01-15 23:48:50 +0800

    refactor: new title-bar component

---
+++ src/renderer/components/Icons/win-minimize.js
@@ -0,0 +1,15 @@
+import Icon from '@/components/Icons/Icon'
+
+Icon.register({
+  'win-minimize': {
+    'width': 12,
+    'height': 12,
+    'raw': `<line x1="1" y1="6" x2="11" y2="6" fill="none" stroke-linecap="round" stroke-linejoin="round"/>`,
+    'g': {
+      'stroke': 'currentColor',
+      'stroke-linecap': 'round',
+      'stroke-linejoin': 'round',
+      'stroke-width': '1'
+    }
+  }
+})
