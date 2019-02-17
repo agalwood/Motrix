37fe7d4dbdbc8c40d9f38e20332b9524e8965157
gaokai <raistlin916@gmail.com>
2019-02-18 01:05:25 +0800

    feat: msg add max length with queue

--- src/renderer/components/Msg/index.js
+++ src/renderer/components/Msg/index.js
@@ -1,15 +1,40 @@
+const queue = []
+const maxLength = 5
+
 export default {
   install: function (Vue, Message, defaultOption = {}) {
     Vue.prototype.$msg = new Proxy(Message, {
       get (obj, prop) {
         return (arg) => {
-          if (arg instanceof String) {
+          if (!(arg instanceof Object)) {
             arg = { message: arg }
           }
-          obj[prop]({
-            ...defaultOption,
-            ...arg
-          })
+          const task = {
+            run () {
+              obj[prop]({
+                ...defaultOption,
+                ...arg,
+                onClose (...data) {
+                  const currentTask = queue.pop()
+                  if (currentTask) {
+                    currentTask.run()
+                  }
+                  if (arg.onClose) {
+                    arg.onClose(...data)
+                  }
+                }
+              })
+            }
+          }
+
+          if (queue.length >= maxLength) {
+            queue.pop()
+          }
+          queue.unshift(task)
+
+          if (queue.length === 1) {
+            queue[0].run()
+          }
         }
       }
     })
