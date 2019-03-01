a645a1de818dfea001175333a4648180bdb16074
Andre Noberto <andre-noberto@hotmail.com>
2019-03-01 15:24:40 -0300

    Added locales for pt-BR

---
+++ src/shared/locales/pt-BR/window.js
@@ -0,0 +1,8 @@
+export default {
+  'reload': 'Recarregar',
+  'close': 'Fechar',
+  'minimize': 'Minimizar',
+  'zoom': 'Zoom',
+  'toggle-fullscreen': 'Modo Full Screen',
+  'front': 'Trazer Tudo Para a Frente'
+}
