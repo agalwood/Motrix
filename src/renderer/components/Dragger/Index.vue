b3686fff04df4116218655f9dbef381ea4343b3c
Dr_rOot <agalwood.net@gmail.com>
2019-03-15 23:32:58 +0800

    fix: torrent dragger on drop

--- src/renderer/components/Dragger/Index.vue
+++ src/renderer/components/Dragger/Index.vue
@@ -22,14 +22,27 @@ export default {
       }
     }
 
+    this.onDrop = (ev) => {
+      count = 0
+
+      const fileList = [...ev.dataTransfer.files]
+        .map(item => ({ raw: item, name: item.name }))
+        .filter(item => /\.torrent$/.test(item.name))
+      if (!fileList.length) {
+        this.$msg.error(this.$t('task.select-torrent'))
+      }
+    }
+
     document.addEventListener('dragover', this.preventDefault)
     document.body.addEventListener('dragenter', this.onDragEnter)
     document.body.addEventListener('dragleave', this.onDragLeave)
+    document.body.addEventListener('drop', this.onDrop)
   },
   destroyed () {
     document.removeEventListener('dragover', this.preventDefault)
     document.body.removeEventListener('dragenter', this.onDragEnter)
     document.body.removeEventListener('dragleave', this.onDragLeave)
+    document.body.removeEventListener('drop', this.onDrop)
   }
 }
 </script>
