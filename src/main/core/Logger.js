359da895e7f09fb36171fee9360cd8fba649230b
Dr_rOot <agalwood.net@gmail.com>
2019-04-23 17:23:52 +0800

    fix: migration electron-log from v2 to v3

--- src/main/core/Logger.js
+++ src/main/core/Logger.js
@@ -1,7 +1,7 @@
 import is from 'electron-is'
 import logger from 'electron-log'
 
-logger.transports.file.level = is.production() ? 'warn' : 'info'
+logger.transports.file.level = is.production() ? 'warn' : 'silly'
 logger.info('Logger init')
 logger.warn('[Motrix] Logger init')
 
