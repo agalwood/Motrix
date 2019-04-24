23a5ddfe1696dcf6ea22824b3ebc475d4d5e128d
HBKRKZK <hbkrkzk@gmail.com>
2019-04-24 14:59:23 +0800

    Update all.js

--- src/shared/locales/all.js
+++ src/shared/locales/all.js
@@ -1,6 +1,7 @@
 import eleLocaleDe from 'element-ui/lib/locale/lang/de'
 import eleLocaleEn from 'element-ui/lib/locale/lang/en'
 import eleLocaleFr from 'element-ui/lib/locale/lang/fr'
+import eleLocaleJa from 'element-ui/lib/locale/lang/ja'
 import eleLocalePtBR from 'element-ui/lib/locale/lang/pt-br'
 import eleLocaleTr from 'element-ui/lib/locale/lang/tr-TR'
 import eleLocaleZhCN from 'element-ui/lib/locale/lang/zh-CN'
@@ -8,6 +9,7 @@ import eleLocaleZhTW from 'element-ui/lib/locale/lang/zh-TW'
 import appLocaleDe from '@shared/locales/de'
 import appLocaleEnUS from '@shared/locales/en-US'
 import appLocaleFr from '@shared/locales/fr'
+import appLocaleJa from '@shared/locales/ja'
 import appLocalePtBR from '@shared/locales/pt-BR'
 import appLocaleTr from '@shared/locales/tr'
 import appLocaleZhCN from '@shared/locales/zh-CN'
@@ -31,6 +33,12 @@ const resources = {
     translation: {
       ...eleLocaleFr,
       ...appLocaleFr
+    }
+  },
+    'ja': {
+    translation: {
+      ...eleLocaleJa,
+      ...appLocaleJa
     }
   },
   'pt-BR': {
