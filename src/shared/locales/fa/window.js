c2f137380c9ea333fe0f4b47abca2eeb4ea466bc
Nima Rasooli <nimarasooli1@gmail.com>
2019-04-26 20:49:03 +0430

    Translated

---
+++ src/shared/locales/fa/window.js
@@ -0,0 +1,8 @@
+export default {
+  'reload': 'ریلود',
+  'close': 'بستن',
+  'minimize': 'کوچک کردن',
+  'zoom': 'زوم',
+  'toggle-fullscreen': 'رفتن به حالت فول‌اسکرین',
+  'front': 'همه را جلو بیار'
+}
