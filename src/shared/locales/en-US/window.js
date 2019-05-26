b086089490a14706643eca1902d6d8725046de0a
Gary Li <i@garyli.me>
2019-05-25 23:12:10 -0700

    Updated translation: en-US

--- src/shared/locales/en-US/window.js
+++ src/shared/locales/en-US/window.js
@@ -2,7 +2,7 @@ export default {
   'reload': 'Reload',
   'close': 'Close',
   'minimize': 'Minimize',
-  'zoom': 'Zoom',
+  'zoom': 'Maximize',
   'toggle-fullscreen': 'Enter Full Screen',
   'front': 'Bring All to Front'
 }
