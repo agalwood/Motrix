24c984223d4c313aabd3d2e8ce4eae17d64eacc5
byoungd <pg98@vip.qq.com>
2020-01-05 14:50:33 +0800

    feat:make Motrix layout more responsive

--- src/main/configs/page.js
+++ src/main/configs/page.js
@@ -6,7 +6,7 @@ export default {
       title: 'Motrix',
       width: 1024,
       height: 768,
-      minWidth: 840,
+      minWidth: 400,
       minHeight: 420,
       // backgroundColor: '#FFFFFF',
       transparent: !is.windows()
--- src/renderer/components/Task/Index.vue
+++ src/renderer/components/Task/Index.vue
@@ -1,11 +1,18 @@
 <template>
-  <el-container class="content panel" direction="horizontal">
-    <el-aside width="200px" class="subnav">
+  <el-container class="content panel"
+                direction="horizontal">
+    <el-aside width="200px"
+              class="subnav hidden-xs-only">
       <mo-task-subnav :current="status" />
     </el-aside>
-    <el-container class="content panel" direction="vertical">
-      <el-header class="panel-header" height="84">
-        <h4>{{ title }}</h4>
+    <el-container class="content panel"
+                  direction="vertical">
+      <el-header class="panel-header"
+                 height="84">
+        <h4 class="task-title hidden-xs-only">{{ title }}</h4>
+        <mo-task-switcher :title="title"
+                          :commands="titles"
+                          class="hidden-sm-and-up" />
         <mo-task-actions />
       </el-header>
       <el-main class="panel-content">
@@ -16,48 +23,60 @@
 </template>
 
 <script>
-  import TaskSubnav from '@/components/Subnav/TaskSubnav'
-  import TaskActions from '@/components/Task/TaskActions'
-  import TaskList from '@/components/Task/TaskList'
+import TaskSubnav from '@/components/Subnav/TaskSubnav'
+import TaskActions from '@/components/Task/TaskActions'
+import TaskList from '@/components/Task/TaskList'
+import TaskSwitch from '@/components/Task/TaskSwitcher'
 
-  export default {
-    name: 'mo-content-task',
-    components: {
-      [TaskSubnav.name]: TaskSubnav,
-      [TaskActions.name]: TaskActions,
-      [TaskList.name]: TaskList
-    },
-    props: {
-      status: {
-        type: String,
-        default: 'active'
-      }
-    },
-    computed: {
-      title: function () {
-        const titles = {
-          'active': this.$t('task.active'),
-          'waiting': this.$t('task.waiting'),
-          'stopped': this.$t('task.stopped')
-        }
-        return titles[this.status]
-      }
-    },
-    watch: {
-      'status': 'onStatusChange'
-    },
-    methods: {
-      onStatusChange () {
-        this.changeCurrentList()
-      },
-      changeCurrentList () {
-        this.$store.dispatch('task/changeCurrentList', this.status)
-      }
-    },
-    created: function () {
-      this.changeCurrentList()
+export default {
+  name: 'mo-content-task',
+  components: {
+    [TaskSubnav.name]: TaskSubnav,
+    [TaskActions.name]: TaskActions,
+    [TaskList.name]: TaskList,
+    [TaskSwitch.name]: TaskSwitch
+  },
+  props: {
+    status: {
+      type: String,
+      default: 'active'
     }
+  },
+  computed: {
+    titleMap: function () {
+      return {
+        active: this.$t('task.active'),
+        waiting: this.$t('task.waiting'),
+        stopped: this.$t('task.stopped')
+      }
+    },
+    titles: function () {
+      return Object.keys(this.titleMap).map(key => {
+        return {
+          key,
+          label: this.titleMap[key]
+        }
+      })
+    },
+    title: function () {
+      return this.titleMap[this.status]
+    }
+  },
+  watch: {
+    status: 'onStatusChange'
+  },
+  methods: {
+    onStatusChange () {
+      this.changeCurrentList()
+    },
+    changeCurrentList () {
+      this.$store.dispatch('task/changeCurrentList', this.status)
+    }
+  },
+  created: function () {
+    this.changeCurrentList()
   }
+}
 </script>
 
 <style lang="scss">
