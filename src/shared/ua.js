bbe52a2a18c490aa079c916c58d090165efce2cc
Dr_rOot <agalwood.net@gmail.com>
2020-01-28 13:54:27 +0800

    chore: update chrome ua

--- src/shared/ua.js
+++ src/shared/ua.js
@@ -1,5 +1,5 @@
 export default {
   'transmission': 'Transmission/2.94',
-  'chrome': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
+  'chrome': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
   'du': 'netdisk;6.0.0.12;PC;PC-Windows;10.0.16299;WindowsBaiduYunGuanJia'
 }
