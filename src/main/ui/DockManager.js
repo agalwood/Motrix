b7356702fc7fab7931df0bfa4ee044dae7d900e2
Dr_rOot <agalwood.net@gmail.com>
2020-01-31 15:10:51 +0800

    feat: add DockManager

---
+++ src/main/ui/DockManager.js
@@ -0,0 +1,36 @@
+import is from 'electron-is'
+import { EventEmitter } from 'events'
+import { app } from 'electron'
+import {
+  APP_RUN_MODE
+} from '@shared/constants'
+
+const isMac = is.macOS()
+
+export default class DockManager extends EventEmitter {
+  constructor (options) {
+    super()
+    this.options = options
+    const { runMode } = this.options
+    console.log('DockManager options', this.options)
+    if (runMode !== APP_RUN_MODE.STANDARD) {
+      this.hide()
+    }
+  }
+
+  show = isMac ? () => {
+    return app.dock.show()
+  } : () => {}
+
+  hide = isMac ? () => {
+    app.dock.hide()
+  } : () => {}
+
+  setBadge = isMac ? (text) => {
+    app.dock.setBadge(text)
+  } : (text) => {}
+
+  openDock = isMac ? (path) => {
+    app.dock.downloadFinished(path)
+  } : (path) => {}
+}
