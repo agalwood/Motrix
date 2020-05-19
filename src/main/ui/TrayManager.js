4d964ae16e1253532d5b481d2b394db307d30747
Dr_rOot <agalwood.net@gmail.com>
2020-05-19 21:50:36 +0800

    fix: tray destroy remove listener

--- src/main/ui/TrayManager.js
+++ src/main/ui/TrayManager.js
@@ -150,6 +150,13 @@ export default class TrayManager extends EventEmitter {
   }
 
   destroy () {
+    if (tray) {
+      tray.removeListener('click', this.handleTrayClick)
+      tray.removeListener('double-click', this.handleTrayDbClick)
+      tray.removeListener('right-click', this.handleTrayRightClick)
+      tray.removeListener('drop-files', this.handleTrayDropFile)
+    }
+
     tray.destroy()
   }
 }
