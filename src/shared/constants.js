e82e5670699d5871ab4ac9b889928eb4e477e206
Dr_rOot <agalwood.net@gmail.com>
2020-05-24 20:15:20 +0800

    refactor: time constants

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -88,11 +88,16 @@ export const trackerSourceOptions = [
   }
 ]
 
+export const ONE_SECOND = 1000
+export const ONE_MINUTE = ONE_SECOND * 60
+export const ONE_HOUR = ONE_MINUTE * 60
+export const ONE_DAY = ONE_HOUR * 24
+
 // 12 Hours
-export const AUTO_SYNC_TRACKER_INTERVAL = 12 * 60 * 60 * 1000
+export const AUTO_SYNC_TRACKER_INTERVAL = ONE_HOUR * 12
 
 // One Week
-export const AUTO_CHECK_UPDATE_INTERVAL = 7 * 24 * 60 * 60 * 1000
+export const AUTO_CHECK_UPDATE_INTERVAL = ONE_DAY * 7
 
 export const NONE_SELECTED_FILES = 'none'
 export const SELECTED_ALL_FILES = 'all'
