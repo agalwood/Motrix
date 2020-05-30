211bb6eb4bfcc2ed1ff1cc29e888fb518e596c32
Duy–Thanh Doan <tech@duythanh.vn>
2020-05-30 20:05:26 +0700

    Translated app.js

--- src/shared/locales/vi/about.js
+++ src/shared/locales/vi/about.js
@@ -1,7 +1,7 @@
 export default {
-    'engine-version': 'Phiên bản Hệ thống',
+    'engine-version': 'Phiên bản Ứng dụng',
     'license': 'Giấy phép',
-    'about': 'Về',
+    'about': 'Về Motrix',
     'release': 'Phát hành',
     'support': 'Hỗ trợ'
   }
