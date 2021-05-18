ad62b8f6762424835b2fafd206eead96b56a84c6
rubjo <rune.bjorneras@gmail.com>
2021-05-18 09:45:54 +0200

    ✏️ Escape apstrophe

--- src/shared/locales/nb-NO/help.js
+++ src/shared/locales/nb-NO/help.js
@@ -1,5 +1,5 @@
 export default {
-  'official-website': 'Motrix' nettside',
+  'official-website': 'Motrix\' nettside',
   'manual': 'Brukerhåndbok',
   'release-notes': 'Utgivelsesnotater...',
   'report-problem': 'Rapporter problem',
