a5752d14db97b95ef7224d50b24a3bebe0fdf090
CDzungx <59353870+CDzungx@users.noreply.github.com>
2021-06-08 18:59:10 +0700

    Update window.js

--- src/shared/locales/vi/window.js
+++ src/shared/locales/vi/window.js
@@ -2,7 +2,7 @@ export default {
   'reload': 'Tải lại',
   'close': 'Đóng',
   'minimize': 'Thu nhỏ',
-  'zoom': 'Phóng to',
-  'toggle-fullscreen': 'Mở toàn màn hình',
+  'zoom': 'Thu phóng',
+  'toggle-fullscreen': 'Mở Toàn Màn Hình',
   'front': 'Đưa tất cả lên phía trước'
 }
