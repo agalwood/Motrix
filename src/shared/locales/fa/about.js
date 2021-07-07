3c3d1a389f2fc3d9f83801ccb5198e336f8904cc
Eshagh <ase.mugem@gmail.com>
2021-07-07 22:26:04 +0430

    Update about.js

--- src/shared/locales/fa/about.js
+++ src/shared/locales/fa/about.js
@@ -1,7 +1,7 @@
 export default {
   'engine-version': 'نسخه‌ی موتور',
-  'license': 'مجوز منبع باز',
-  'about': 'درباره ما',
+  'license': 'مجوز',
+  'about': 'دربارهٔ ما',
   'release': 'نسخه‌های منتشر شده',
   'support': 'حمایت'
 }
