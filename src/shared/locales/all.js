9c93d58cde96756a04ad9ef43864dce5fa00373b
Nick Bouwhuis <nick@bouwhuis.io>
2021-11-28 12:18:47 +0100

    add 'nl' to `src/shared/locales/app.js` and `src/shared/locales/all.js`

--- src/shared/locales/all.js
+++ src/shared/locales/all.js
@@ -13,6 +13,7 @@ import elelocaleIt from 'element-ui/lib/locale/lang/it'
 import eleLocaleJa from 'element-ui/lib/locale/lang/ja'
 import eleLocaleKo from 'element-ui/lib/locale/lang/ko'
 import eleLocaleNb from 'element-ui/lib/locale/lang/nb-NO'
+import eleLocaleNl from 'element-ui/lib/locale/lang/nl'
 import eleLocalePl from 'element-ui/lib/locale/lang/pl'
 import eleLocalePtBR from 'element-ui/lib/locale/lang/pt-br'
 import eleLocaleRo from 'element-ui/lib/locale/lang/ro'
@@ -37,6 +38,7 @@ import applocaleIt from '@shared/locales/it'
 import appLocaleJa from '@shared/locales/ja'
 import appLocaleKo from '@shared/locales/ko'
 import appLocaleNb from '@shared/locales/nb'
+import appLocaleNl from '@shared/locales/nl'
 import appLocalePl from '@shared/locales/pl'
 import appLocalePtBR from '@shared/locales/pt-BR'
 import appLocaleRo from '@shared/locales/ro'
@@ -50,151 +52,157 @@ import appLocaleZhTW from '@shared/locales/zh-TW'
 // Please keep the locale key in alphabetical order.
 /* eslint-disable quote-props */
 const resources = {
-  'ar': {
-    translation: {
-      ...eleLocaleAr,
-      ...appLocaleAr
+        'ar': {
+            translation: {
+                ...eleLocaleAr,
+                ...appLocaleAr
+            }
+        },
+        'bg': {
+            translation: {
+                ...eleLocaleBg,
+                ...appLocaleBg
+            }
+        },
+        'ca': {
+            translation: {
+                ...eleLocaleCa,
+                ...appLocaleCa
+            }
+        },
+        'de': {
+            translation: {
+                ...eleLocaleDe,
+                ...appLocaleDe
+            }
+        },
+        'el': {
+            translation: {
+                ...eleLocaleEl,
+                ...appLocaleEl
+            }
+        },
+        'en-US': {
+            translation: {
+                ...eleLocaleEn,
+                ...appLocaleEnUS
+            }
+        },
+        'es': {
+            translation: {
+                ...eleLocaleEs,
+                ...appLocaleEs
+            }
+        },
+        'fa': {
+            translation: {
+                ...eleLocaleFa,
+                ...appLocaleFa
+            }
+        },
+        'fr': {
+            translation: {
+                ...eleLocaleFr,
+                ...appLocaleFr
+            }
+        },
+        'hu': {
+            translation: {
+                ...eleLocaleHu,
+                ...appLocaleHu
+            }
+        },
+        'id': {
+            translation: {
+                ...eleLocaleId,
+                ...appLocaleId
+            }
+        },
+        'it': {
+            translation: {
+                ...elelocaleIt,
+                ...applocaleIt
+            }
+        },
+        'ja': {
+            translation: {
+                ...eleLocaleJa,
+                ...appLocaleJa
+            }
+        },
+        'ko': {
+            translation: {
+                ...eleLocaleKo,
+                ...appLocaleKo
+            }
+        },
+        'nb': {
+            translation: {
+                ...eleLocaleNb,
+                ...appLocaleNb
+            }
+        },
+        'nl': {
+            translation: {
+                ...eleLocaleNl,
+                ...appLocaleNl
+            }
+        },
+        'pl': {
+            translation: {
+                ...eleLocalePl,
+                ...appLocalePl
+            }
+        },
+        'pt-BR': {
+            translation: {
+                ...eleLocalePtBR,
+                ...appLocalePtBR
+            }
+        },
+        'ro': {
+            translation: {
+                ...eleLocaleRo,
+                ...appLocaleRo
+            }
+        },
+        'ru': {
+            translation: {
+                ...eleLocaleRu,
+                ...appLocaleRu
+            }
+        },
+        'tr': {
+            translation: {
+                ...eleLocaleTr,
+                ...appLocaleTr
+            }
+        },
+        'uk': {
+            translation: {
+                ...eleLocaleUk,
+                ...appLocaleUk
+            }
+        },
+        'vi': {
+            translation: {
+                ...eleLocaleVi,
+                ...appLocaleVi
+            }
+        },
+        'zh-CN': {
+            translation: {
+                ...eleLocaleZhCN,
+                ...appLocaleZhCN
+            }
+        },
+        'zh-TW': {
+            translation: {
+                ...eleLocaleZhTW,
+                ...appLocaleZhTW
+            }
+        }
     }
-  },
-  'bg': {
-    translation: {
-      ...eleLocaleBg,
-      ...appLocaleBg
-    }
-  },
-  'ca': {
-    translation: {
-      ...eleLocaleCa,
-      ...appLocaleCa
-    }
-  },
-  'de': {
-    translation: {
-      ...eleLocaleDe,
-      ...appLocaleDe
-    }
-  },
-  'el': {
-    translation: {
-      ...eleLocaleEl,
-      ...appLocaleEl
-    }
-  },
-  'en-US': {
-    translation: {
-      ...eleLocaleEn,
-      ...appLocaleEnUS
-    }
-  },
-  'es': {
-    translation: {
-      ...eleLocaleEs,
-      ...appLocaleEs
-    }
-  },
-  'fa': {
-    translation: {
-      ...eleLocaleFa,
-      ...appLocaleFa
-    }
-  },
-  'fr': {
-    translation: {
-      ...eleLocaleFr,
-      ...appLocaleFr
-    }
-  },
-  'hu': {
-    translation: {
-      ...eleLocaleHu,
-      ...appLocaleHu
-    }
-  },
-  'id': {
-    translation: {
-      ...eleLocaleId,
-      ...appLocaleId
-    }
-  },
-  'it': {
-    translation: {
-      ...elelocaleIt,
-      ...applocaleIt
-    }
-  },
-  'ja': {
-    translation: {
-      ...eleLocaleJa,
-      ...appLocaleJa
-    }
-  },
-  'ko': {
-    translation: {
-      ...eleLocaleKo,
-      ...appLocaleKo
-    }
-  },
-  'nb': {
-    translation: {
-      ...eleLocaleNb,
-      ...appLocaleNb
-    }
-  },
-  'pl': {
-    translation: {
-      ...eleLocalePl,
-      ...appLocalePl
-    }
-  },
-  'pt-BR': {
-    translation: {
-      ...eleLocalePtBR,
-      ...appLocalePtBR
-    }
-  },
-  'ro': {
-    translation: {
-      ...eleLocaleRo,
-      ...appLocaleRo
-    }
-  },
-  'ru': {
-    translation: {
-      ...eleLocaleRu,
-      ...appLocaleRu
-    }
-  },
-  'tr': {
-    translation: {
-      ...eleLocaleTr,
-      ...appLocaleTr
-    }
-  },
-  'uk': {
-    translation: {
-      ...eleLocaleUk,
-      ...appLocaleUk
-    }
-  },
-  'vi': {
-    translation: {
-      ...eleLocaleVi,
-      ...appLocaleVi
-    }
-  },
-  'zh-CN': {
-    translation: {
-      ...eleLocaleZhCN,
-      ...appLocaleZhCN
-    }
-  },
-  'zh-TW': {
-    translation: {
-      ...eleLocaleZhTW,
-      ...appLocaleZhTW
-    }
-  }
-}
-/* eslint-enable quote-props */
+    /* eslint-enable quote-props */
 
 export default resources
--- src/shared/locales/app.js
+++ src/shared/locales/app.js
@@ -10,6 +10,7 @@ import appLocaleHu from '@shared/locales/hu'
 import appLocaleId from '@shared/locales/id'
 import appLocaleIt from '@shared/locales/it'
 import appLocaleJa from '@shared/locales/ja'
+import appLocaleNl from '@shared/locales/nl'
 import appLocaleKo from '@shared/locales/ko'
 import appLocalePl from '@shared/locales/pl'
 import appLocalePtBR from '@shared/locales/pt-BR'
@@ -24,117 +25,122 @@ import appLocaleZhTW from '@shared/locales/zh-TW'
 // Please keep the locale key in alphabetical order.
 /* eslint-disable quote-props */
 const resources = {
-  'ar': {
-    translation: {
-      ...appLocaleAr
+        'ar': {
+            translation: {
+                ...appLocaleAr
+            }
+        },
+        'bg': {
+            translation: {
+                ...appLocaleBg
+            }
+        },
+        'ca': {
+            translation: {
+                ...appLocaleCa
+            }
+        },
+        'de': {
+            translation: {
+                ...appLocaleDe
+            }
+        },
+        'el': {
+            translation: {
+                ...appLocaleEl
+            }
+        },
+        'en-US': {
+            translation: {
+                ...appLocaleEnUS
+            }
+        },
+        'fa': {
+            translation: {
+                ...appLocaleFa
+            }
+        },
+        'fr': {
+            translation: {
+                ...appLocaleFr
+            }
+        },
+        'hu': {
+            translation: {
+                ...appLocaleHu
+            }
+        },
+        'id': {
+            translation: {
+                ...appLocaleId
+            }
+        },
+        'it': {
+            translation: {
+                ...appLocaleIt
+            }
+        },
+        'ja': {
+            translation: {
+                ...appLocaleJa
+            }
+        },
+        'nl': {
+            translation: {
+                ...appLocaleNl
+            }
+        },
+        'ko': {
+            translation: {
+                ...appLocaleKo
+            }
+        },
+        'pl': {
+            translation: {
+                ...appLocalePl
+            }
+        },
+        'pt-BR': {
+            translation: {
+                ...appLocalePtBR
+            }
+        },
+        'ro': {
+            translation: {
+                ...appLocaleRo
+            }
+        },
+        'ru': {
+            translation: {
+                ...appLocaleRu
+            }
+        },
+        'tr': {
+            translation: {
+                ...appLocaleTr
+            }
+        },
+        'uk': {
+            translation: {
+                ...appLocaleUk
+            }
+        },
+        'vi': {
+            translation: {
+                ...appLocaleVi
+            }
+        },
+        'zh-CN': {
+            translation: {
+                ...appLocaleZhCN
+            }
+        },
+        'zh-TW': {
+            translation: {
+                ...appLocaleZhTW
+            }
+        }
     }
-  },
-  'bg': {
-    translation: {
-      ...appLocaleBg
-    }
-  },
-  'ca': {
-    translation: {
-      ...appLocaleCa
-    }
-  },
-  'de': {
-    translation: {
-      ...appLocaleDe
-    }
-  },
-  'el': {
-    translation: {
-      ...appLocaleEl
-    }
-  },
-  'en-US': {
-    translation: {
-      ...appLocaleEnUS
-    }
-  },
-  'fa': {
-    translation: {
-      ...appLocaleFa
-    }
-  },
-  'fr': {
-    translation: {
-      ...appLocaleFr
-    }
-  },
-  'hu': {
-    translation: {
-      ...appLocaleHu
-    }
-  },
-  'id': {
-    translation: {
-      ...appLocaleId
-    }
-  },
-  'it': {
-    translation: {
-      ...appLocaleIt
-    }
-  },
-  'ja': {
-    translation: {
-      ...appLocaleJa
-    }
-  },
-  'ko': {
-    translation: {
-      ...appLocaleKo
-    }
-  },
-  'pl': {
-    translation: {
-      ...appLocalePl
-    }
-  },
-  'pt-BR': {
-    translation: {
-      ...appLocalePtBR
-    }
-  },
-  'ro': {
-    translation: {
-      ...appLocaleRo
-    }
-  },
-  'ru': {
-    translation: {
-      ...appLocaleRu
-    }
-  },
-  'tr': {
-    translation: {
-      ...appLocaleTr
-    }
-  },
-  'uk': {
-    translation: {
-      ...appLocaleUk
-    }
-  },
-  'vi': {
-    translation: {
-      ...appLocaleVi
-    }
-  },
-  'zh-CN': {
-    translation: {
-      ...appLocaleZhCN
-    }
-  },
-  'zh-TW': {
-    translation: {
-      ...appLocaleZhTW
-    }
-  }
-}
-/* eslint-enable quote-props */
+    /* eslint-enable quote-props */
 
 export default resources
