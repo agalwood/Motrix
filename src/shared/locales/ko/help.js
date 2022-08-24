07d0ce91de95e3c8d2d8fb9245a2cb48159f7ce8
Minseo Lee <itoupluk427@gmail.com>
2022-08-24 15:50:30 +0900

    Update help.js

--- src/shared/locales/ko/help.js
+++ src/shared/locales/ko/help.js
@@ -1,7 +1,7 @@
 export default {
-  'official-website': 'Motrix 웹사이트',
-  'manual': '메뉴얼',
-  'release-notes': '배포 정보...',
+  'official-website': 'Motrix 웹 사이트',
+  'manual': '매뉴얼',
+  'release-notes': '릴리스 정보...',
   'report-problem': '문제 보고',
   'toggle-dev-tools': '개발자 도구'
 }
