65bf25dc1a7652fa0fa86862035b06e8ba610c5d
Nawapon Boonjua <79404127+nxanywhere@users.noreply.github.com>
2023-03-24 20:32:19 +0700

    feat: Translated to Thai Language (#1046)

---
+++ src/shared/locales/th-TH/help.js
@@ -0,0 +1,7 @@
+export default {
+  'official-website': 'เว็บไซต์ Motrix',
+  'manual': 'คู่มือ',
+  'release-notes': 'บันทึกประจำรุ่น...',
+  'report-problem': 'รายงานปัญหา',
+  'toggle-dev-tools': 'สลับไปยังเครื่องมือสำหรับนักพัฒนา'
+}
---
+++ src/shared/locales/th-TH/window.js
@@ -0,0 +1,8 @@
+export default {
+  'reload': 'โหลดใหม่',
+  'close': 'ปิด',
+  'minimize': 'ย่อหน้าต่างเล็ก',
+  'zoom': 'ขยาย',
+  'toggle-fullscreen': 'เข้าสู่เต็มหน้าจอ',
+  'front': 'นำทั้งหมดมาไว้ข้างหน้า'
+}
